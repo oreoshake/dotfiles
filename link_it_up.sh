74a76d8f1b834f95a631579122a50993e57524b8
Neil Matatall <oreoshake@users.noreply.github.com>
2015-05-01 13:30:27 -0700

    add .gemrc

--- link_it_up.sh
+++ link_it_up.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-for FILE in .vimrc .bash_profile .git-prompt.sh .git-completion.bash .gitconfig .inputrc .functions
+for FILE in .vimrc .bash_profile .git-prompt.sh .git-completion.bash .gitconfig .inputrc .functions .gemrc .git-completion.bash
 do
   ln -sfhv ~/dotfiles/$FILE ~
 done

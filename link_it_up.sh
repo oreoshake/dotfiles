2b51c9dc2fa8dfe267a54384c64b0035f9a992e9
Neil Matatall <oreoshake@github.com>
2017-02-24 09:11:47 -1000

    Update link_it_up.sh

--- link_it_up.sh
+++ link_it_up.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-
+poooooo
 for FILE in .vimrc .bash_profile .git-prompt.sh .git-completion.bash .gitconfig .inputrc .functions .gemrc .git-completion.bash
 do
   ln -sfhv ~/dotfiles/$FILE ~
